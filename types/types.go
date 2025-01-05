525bfa44965fae868c4c6ceb8eb50b156bd56028
aksh16 <akshpatel161@gmail.com>
2025-01-04 17:57:13 -0700

    payload structure

--- types/types.go
+++ types/types.go
@@ -1,1 +1,14 @@
 package types
+
+type Item struct {
+	shortDescription string `json:"shortDescription"`
+	price            string `json:"shortDescription"`
+}
+
+type ReceiptPayload struct {
+	retailer     string `json:"retailer"`
+	purchaseDate string `json:"purchaseDate"`
+	purchaseTime string `json:"purchaseTime"`
+	items        []Item `json:"items"`
+	total        string `json:total`
+}
